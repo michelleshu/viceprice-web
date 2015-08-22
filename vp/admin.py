d6340ee4228eeda5c47dd80f03579e7fefe78b2b
Michelle Shu <shu.michelle.w@gmail.com>
2015-08-22 18:13:42 -0400

    Register location domain objects with Django admin

--- vp/admin.py
+++ vp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from vp.models import *
 
-# Register your models here.
+admin.site.register(LocationType)
+admin.site.register(ProductCategory)
+admin.site.register(BusinessHour)
+admin.site.register(Address)
+admin.site.register(Location)
