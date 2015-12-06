e967f05f472b2d0b25d31e551bfe9ac1c43534cb
Michelle Shu <shu.michelle.w@gmail.com>
2015-12-05 23:40:27 -0500

    Add clock process for task scheduling

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn viceprice.wsgi --log-file -
+clock: python clock.py
---
+++ clock.py
@@ -0,0 +1,15 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+import pytz
+
+sched = BlockingScheduler()
+eastern_timezone = pytz.timezone('US/Eastern')
+
+@sched.scheduled_job('cron', minute='5-55/10', second='0', timezone=eastern_timezone)
+def queue_mturk_website_update():
+    print('MTurk website update queued')
+
+@sched.scheduled_job('cron', minute='0.30', second='0', timezone=eastern_timezone)
+def queue_mturk_phone_update():
+    print('MTurk phone update queued')
+
+sched.start()
