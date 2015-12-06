f96050a3f1363465e8f056d0570a3a80f51c1bec
Michelle Shu <shu.michelle.w@gmail.com>
2015-12-05 23:54:27 -0500

    Add worker thread

--- clock.py
+++ clock.py
@@ -1,15 +1,23 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from rq import Queue
+from worker import conn
+from vp.management.commands.updatemturkdata import printUpdate
 import pytz
 
 sched = BlockingScheduler()
 eastern_timezone = pytz.timezone('US/Eastern')
+q = Queue(connection = conn)
 
 @sched.scheduled_job('cron', minute='5-55/10', second='0', timezone=eastern_timezone)
 def queue_mturk_website_update():
     print('MTurk website update queued')
+    result = q.enqueue(printUpdate, True);
+    print(result)
 
 @sched.scheduled_job('cron', minute='*/30', second='0', timezone=eastern_timezone)
 def queue_mturk_phone_update():
     print('MTurk phone update queued')
+    result = q.enqueue(printUpdate, False);
+    print(result)
 
 sched.start()
---
+++ vp/management/commands/updatemturkdata.py
@@ -0,0 +1,5 @@
+def printUpdate(isWebsite):
+    if isWebsite:
+        print("Website update started")
+    else:
+        print("Phone update started")
