380c83907b45ee573778aafd4883f8f2190e7595
Michelle Shu <shu.michelle.w@gmail.com>
2015-12-06 14:25:35 -0500

    Add basic logging

--- clock.py
+++ clock.py
@@ -3,19 +3,21 @@ from rq import Queue
 from worker import conn
 from vp.management.commands import updatemturkweb, updatemturkphone
 import pytz
+import logging
 
 sched = BlockingScheduler()
+logging.basicConfig()
 eastern_timezone = pytz.timezone('US/Eastern')
 q = Queue(connection = conn)
 
 @sched.scheduled_job('cron', minute='5-55/10', second='0', timezone=eastern_timezone)
 def queue_mturk_website_update():
     print('MTurk website update queued')
-    q.enqueue(updatemturkweb.run_website_update())
+    q.enqueue(updatemturkweb.run_website_update)
 
 @sched.scheduled_job('cron', minute='*/30', second='0', timezone=eastern_timezone)
 def queue_mturk_phone_update():
     print('MTurk phone update queued')
-    q.enqueue(updatemturkphone.run_phone_update())
+    q.enqueue(updatemturkphone.run_phone_update)
 
 sched.start()
