b2764e8a817849cc0797a2be6b8b6b4c8ac0e684
Michelle Shu <shu.michelle.w@gmail.com>
2015-12-07 22:51:27 -0500

    stop processes

--- vp/management/commands/disablehits.py
+++ vp/management/commands/disablehits.py
@@ -1,4 +1,13 @@
-conn = connection.MTurkConnection(aws_access_key_id=ACCESS_ID, aws_secret_access_key=SECRET_KEY, host=HOST)
-hits = list(conn.get_all_hits())
-for hit in hits:
-    conn.disable_hit(hit.HITId)
+from django.core.management.base import BaseCommand
+from boto.mturk import connection
+from vp.mturk.mturk_utilities import *
+
+class Command(BaseCommand):
+    help = 'Runs Mechanical Turk tasks for all website locations that require updates and writes completed info to db'
+    args = '<site>'
+
+    def handle(self, *args, **options):
+        conn = connection.MTurkConnection(aws_access_key_id=ACCESS_ID, aws_secret_access_key=SECRET_KEY, host=HOST)
+        hits = list(conn.get_all_hits())
+        for hit in hits:
+            conn.disable_hit(hit.HITId)
