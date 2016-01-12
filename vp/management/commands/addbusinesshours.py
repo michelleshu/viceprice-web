98a50372bb3e2c6560576e9e385aafa56d231123
Michelle Shu <shu.michelle.w@gmail.com>
2016-01-11 21:55:47 -0500

    Add business hours script

--- vp/management/commands/addbusinesshours.py
+++ vp/management/commands/addbusinesshours.py
@@ -56,12 +56,18 @@ class Command(BaseCommand):
                         end_day = start_day
 
                     if row[3] != None and row[3] != '':
-                        start_time = datetime.datetime.strptime(row[3], '%H:%M').time()
+                        start_string = row[3]
+                        if len(start_string) == 4:
+                            start_string = "0" + start_string
+                        start_time = datetime.datetime.strptime(start_string, '%H:%M').time()
                     else:
                         continue
 
                     if row[4] != None and row[4] != '':
-                        end_time = datetime.datetime.strptime(row[4], '%H:%M').time()
+                        end_string = row[4]
+                        if len(end_string) == 4:
+                            end_string = "0" + end_string
+                        end_time = datetime.datetime.strptime(end_string, '%H:%M').time()
                     else:
                         continue
 
