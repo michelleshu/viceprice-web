7734670c28391e3b68153d86684b353cb9c98447
Michelle Shu <shu.michelle.w@gmail.com>
2016-01-23 11:16:54 -0500

    Remove data files from code

--- vp/management/commands/testmturk.py
+++ vp/management/commands/testmturk.py
@@ -3,10 +3,11 @@ from optparse import make_option
 from vp.models import MTurkLocationInfo
 from random import randint
 
-NAME = 'VicePrice Test HIT'
-ADDRESS = '1234 1st St\nWashington, D.C. 20003'
-DEFAULT_URL = 'http://www.website.com'
-DEFAULT_PHONE_NUMBER = '(111)222-3333'
+TEST_LOCATION_ID =
+NAME = 'Liberty Lounge'
+ADDRESS = '3257 Stanton Rd SE,\nWashington, D.C. 20020'
+DEFAULT_URL = 'http://www.justinhinh.com'
+DEFAULT_PHONE_NUMBER = '(202)790-4414'
 DEFAULT_STAGE = 1
 
 class Command(BaseCommand):
@@ -30,25 +31,22 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        mturk_location = self.add_mturk_location(options)
+        mturk_location = self.create_mturk_location(options)
 
 
-    def add_mturk_location(self, options):
+    def create_mturk_location(self, options):
         mturk_location = MTurkLocationInfo(
             foursquare_id = 'RANDID' + str(randint(100, 999)),
             name = NAME,
             address = ADDRESS,
             url = DEFAULT_URL,
-            url_provided = True,
             phone_number = DEFAULT_PHONE_NUMBER,
             stage = DEFAULT_STAGE,
-            get_hh_attempts = 0,
-            deals_confirmations = 0
+            confirmations = 0
         )
 
         if (hasattr(options, 'no-url') and options['no-url']):
             mturk_location.url = None
-            mturk_location.url_provided = False
 
         if (hasattr(options, 'no-phone-number') and options['no-phone-number']):
             mturk_location.phone_number = None
@@ -56,10 +54,7 @@ class Command(BaseCommand):
         if (hasattr(options, 'stage')):
             mturk_location.stage = options['stage']
 
-        if (hasattr(options, 'attempts')):
-            mturk_location.get_hh_attempts = options['attempts']
-
         if (hasattr(options, 'confirmations')):
-            mturk_location.deals_confirmations = options['confirmations']
+            mturk_location.confirmations = options['confirmations']
 
         return mturk_location
