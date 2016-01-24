ce44b6096ac8e389a654853cd34069baaaa9cf63
Michelle Shu <shu.michelle.w@gmail.com>
2016-01-24 12:31:33 -0500

    Add tests for getting locations and mturklocationinfos

--- vp/management/commands/disablehits.py
+++ vp/management/commands/disablehits.py
@@ -1,13 +1,18 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from boto.mturk import connection
-from vp.mturk.mturk_utilities import *
 
 class Command(BaseCommand):
-    help = 'Runs Mechanical Turk tasks for all website locations that require updates and writes completed info to db'
+    help = 'Disable all active HITs'
     args = '<site>'
 
     def handle(self, *args, **options):
-        conn = connection.MTurkConnection(aws_access_key_id=ACCESS_ID, aws_secret_access_key=SECRET_KEY, host=HOST)
+        conn = connection.MTurkConnection(
+            aws_access_key_id = settings.AWS_ACCESS_KEY,
+            aws_secret_access_key = settings.AWS_SECRET_KEY,
+            host = settings.MTURK_HOST)
+
         hits = list(conn.get_all_hits())
+
         for hit in hits:
             conn.disable_hit(hit.HITId)
