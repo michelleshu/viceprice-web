62ac6c673e8e832762788d9a8b97b97d9c70c64e
Michelle Shu <shu.michelle.w@gmail.com>
2016-02-13 15:01:56 -0500

    Add utility method and test for get happy hour found

--- vp/mturk/mturk_update_website_tasks.py
+++ vp/mturk/mturk_update_website_tasks.py
@@ -30,7 +30,8 @@ def update():
                 create_hit(conn, mturk_location, settings.MTURK_HIT_TYPES[FIND_WEBSITE])
             elif (mturk_location.stage == MTURK_STAGE[FIND_HAPPY_HOUR_WEB]):
                 create_hit(conn, mturk_location, settings.MTURK_HIT_TYPES[FIND_HAPPY_HOUR_WEB])
-            #elif (mturk_location.stage == MTURK_STAGE[CONFIRM_HAPPY_HOUR_WEB]):
+            elif (mturk_location.stage == MTURK_STAGE[CONFIRM_HAPPY_HOUR_WEB]):
+                create_hit(conn, mturk_location, settings.MTURK_HIT_TYPES[CONFIRM_HAPPY_HOUR_WEB])
 
         # Evaluate the corresponding HIT assignments for this location if all assignments are complete
         hit = conn.get_hit(mturk_location.hit_id)[0]
@@ -42,60 +43,65 @@ def update():
                 url_agreement_info = process_find_website_hit_assignments(mturk_location, assignments)
                 agreement_percentage = url_agreement_info[0]
                 agreed_url = url_agreement_info[1]
-                print("Agreement Percentage")
-                print(agreement_percentage)
-                print("Agreed URL")
-                print(agreed_url)
 
                 if (agreement_percentage < MIN_AGREEMENT_PERCENTAGE):
                     # Extend the HIT as long as possible to get agreement
                     if len(assignments) < MAX_ASSIGNMENTS_TO_PUBLISH:
-                        print("Extend HIT")
                         conn.extend_hit(hit.HITId, assignments_increment=1)
                     # If exceeds max assignment number allowed by Amazon, we must make a new HIT
                     else:
-                        print("Recreate HIT")
                         conn.disable_hit(hit.HITId)
                         create_hit(conn, mturk_location, settings.MTURK_HIT_TYPES[FIND_WEBSITE])
                 else:
                     # If no URL found, continue to phone stage if possible. Forfeit if not.
                     if (agreed_url == '' or agreed_url == None):
                         if (mturk_location.phone_number != None and mturk_location.phone_number != ''):
-                            print("Go to Phone")
                             mturk_location.hit_id = None
                             mturk_location.stage = MTURK_STAGE[FIND_HAPPY_HOUR_PHONE]
                         else:
-                            print("Go to No Info")
                             mturk_location.stage = MTURK_STAGE[NO_INFO]
                             mturk_location.update_completed = timezone.now()
 
                     else:
-                        print("Go to Find Happy Hour")
                         mturk_location.website = agreed_url
                         mturk_location.stage = MTURK_STAGE[FIND_HAPPY_HOUR_WEB]
                         create_hit(conn, mturk_location, settings.MTURK_HIT_TYPES[FIND_HAPPY_HOUR_WEB])
 
                     approve_and_dispose(conn, hit)
 
-            # elif int(location.stage) == MTURK_STAGE[FIND_WEBSITE_HH]:
-            #     happy_hour_found = was_website_hh_found(conn, location, assignments[-1])
-            #
-            #     if not happy_hour_found:
-            #         if location.get_hh_attempts < MAX_GET_HH_ATTEMPTS:
-            #             conn.extend_hit(hit.HITId, assignments_increment=1)
-            #         else:
-            #             location.hit_id = None
-            #             location.stage = MTURK_STAGE[FIND_PHONE_HH]
-            #             approve_and_dispose(conn, hit)
-            #     else:
-            #         processed = process_find_happy_hour_info_assignment(conn, location, assignments[-1])
-            #
-            #         if processed == True:
-            #             location.stage = MTURK_STAGE[CONFIRM_WEBSITE_HH]
-            #             create_hit(conn, location, HIT_TYPES[CONFIRM_WEBSITE_HH])
-            #             approve_and_dispose(conn, hit)
-            #         else:
-            #             conn.extend_hit(hit.HITId, assignments_increment=1)
+            elif int(mturk_location.stage) == MTURK_STAGE[FIND_HAPPY_HOUR_WEB]:
+                # Get latest assignment
+                assignment = assignments[-1]
+
+                happy_hour_found = get_happy_hour_found(conn, mturk_location, assignment)
+
+                if not happy_hour_found:
+                    # If happy hour was not found because we have the wrong website or wrong phone number, done.
+                    if (mturk_location.stage == MTURK_STAGE[WRONG_WEBSITE] or
+                        mturk_location.stage == MTURK_STAGE[WRONG_PHONE_NUMBER]):
+                        approve_and_dispose(conn, hit)
+
+                    else:
+                        # If we've not maxed out on attempts to get the happy hour info, try again
+                        if mturk_location.attempts < MAX_GET_HAPPY_HOUR_WEB_ATTEMPTS:
+                            conn.extend_hit(hit.HITId, assignments_increment=1)
+                        else:
+                            # Otherwise, transfer to phone process
+                            mturk_location.hit_id = None
+                            mturk_location.stage = MTURK_STAGE[FIND_HAPPY_HOUR_PHONE]
+                            mturk_location.attempts = 0
+                            approve_and_dispose(conn, hit)
+
+                else:
+                    # Happy hour was found. Process response
+                    processed = process_find_happy_hour_info_assignment(conn, location, assignments[-1])
+
+                    if processed == True:
+                        location.stage = MTURK_STAGE[CONFIRM_WEBSITE_HH]
+                        create_hit(conn, location, HIT_TYPES[CONFIRM_WEBSITE_HH])
+                        approve_and_dispose(conn, hit)
+                    else:
+                        conn.extend_hit(hit.HITId, assignments_increment=1)
             #
             # elif int(location.stage) == MTURK_STAGE[CONFIRM_WEBSITE_HH]:
             #     confirmed = process_confirm_happy_hour_info_assignment(conn, location, assignments[-1])
