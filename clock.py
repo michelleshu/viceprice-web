f9c638301a805fde352e1d3ce8f1dda268b090b0
Michelle Shu <shu.michelle.w@gmail.com>
2016-03-13 22:29:00 -0400

    Update cron for phone

--- clock.py
+++ clock.py
@@ -15,7 +15,7 @@ def queue_mturk_website_update():
     print('MTurk website update queued')
     q.enqueue(updatemturkweb.run)
 
-@sched.scheduled_job('cron', hour='11-19', minute='*/30', second='0', timezone=eastern_timezone)
+@sched.scheduled_job('cron', minute='*/30', second='0', timezone=eastern_timezone)
 def queue_mturk_phone_update():
     print('MTurk phone update queued')
     q.enqueue(updatemturkphone.run)
