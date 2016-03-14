63eda963b97595760cde2ca2fbbcbbd393d0971f
Michelle Shu <shu.michelle.w@gmail.com>
2016-03-13 22:21:55 -0400

    Add env variables for HIT Layouts Production

--- clock.py
+++ clock.py
@@ -10,14 +10,14 @@ logging.basicConfig()
 eastern_timezone = pytz.timezone('US/Eastern')
 q = Queue(connection = conn)
 
-@sched.scheduled_job('cron', minute='1', second='0', timezone=eastern_timezone) #@sched.scheduled_job('cron', minute='5-55/10', second='0', timezone=eastern_timezone)
-def queue_mturk_website_update():
-    print('MTurk website update queued')
-    q.enqueue(updatemturkweb.run)
-
+# @sched.scheduled_job('cron', minute='5-55/10', second='0', timezone=eastern_timezone)
+# def queue_mturk_website_update():
+#     print('MTurk website update queued')
+#     q.enqueue(updatemturkweb.run)
+#
 # @sched.scheduled_job('cron', hour='11-19', minute='*/30', second='0', timezone=eastern_timezone)
 # def queue_mturk_phone_update():
 #     print('MTurk phone update queued')
 #     q.enqueue(updatemturkphone.run)
-
-sched.start()
+#
+# sched.start()
