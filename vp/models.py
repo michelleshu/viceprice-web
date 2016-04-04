e768394189bbeabde2ab4a4fe7998b5065925fe4
jeremy <jeremy.a.heaton@gmail.com>
2016-04-03 22:29:47 -0400

    removing enum field since it needs to be tested more

--- vp/models.py
+++ vp/models.py
@@ -79,7 +79,8 @@ class DealType(enum.Enum):
 class DealDetail(models.Model):
     drinkName = models.CharField(max_length=1000)
     drinkCategory = models.IntegerField()
-    detailType = enum.EnumField(DealType, default=DealType.price_off)
+    detailType = models.IntegerField()
+#     detailType = enum.EnumField(DealType, default=DealType.price_off)
     value = models.FloatField()
     
     # Information about a deal at a location
