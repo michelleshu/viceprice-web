64f7d2b8014527741b7e241b9e26e00fa8f3d3dc
Michelle Shu <shu.michelle.w@gmail.com>
2016-05-08 12:34:53 -0400

    Submit drink names selected

--- vp/static/js/confirm-drink-name.js
+++ vp/static/js/confirm-drink-name.js
@@ -1,5 +1,56 @@
+var dealID;
+var itemIDs = [];
+
 $(document).ready(function() {
     get_deal_that_needs_confirmation();
+
+    $("#submit-button").click(function(event) {
+
+        event.preventDefault();
+        event.stopPropagation();
+
+        var namesSelected = [];
+        for (var i  = 0; i < itemIDs.length; i++) {
+            var id = itemIDs[i];
+            var name = $("input[type=radio][name='" + id  + "']:checked").val();
+            if (name == "custom") {
+                name = $("input[type=text][name='" + id  + "']").val();
+            }
+
+            namesSelected.push({
+                "dealDetailID": id,
+                "name": name
+            });
+        }
+
+        var data = {
+            "dealID": dealID,
+            "namesSelected": namesSelected
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/submit_drink_names/",
+            data: JSON.stringify(data),
+            success: function(data) {
+                $(".deal-data").remove();
+                get_deal_that_needs_confirmation();
+            },
+            error: function() {
+                alert("Failed to submit names");
+            }
+        })
+    })
+});
+
+$(document).on("focus", "input[type='text']", function() {
+    $(this).parent().closest('.deal-data').find("input[type='radio']").prop("checked", false);
+    $(this).prev().prop("checked", true);
+});
+
+$(document).on("click", ".drink-name-label", function() {
+    $(this).parent().closest('.deal-data').find("input[type='radio']").prop("checked", false);
+    $(this).prev().prop("checked", true);
 });
 
 var get_deal_that_needs_confirmation = function() {
@@ -7,21 +58,27 @@ var get_deal_that_needs_confirmation = function() {
         type: "GET",
         url: "/get_deal_that_needs_confirmation",
 
-        success: function(data){
+        success: function(data) {
             var dealDetailData = data["deal_detail_data"];
+            dealID = data["deal_id"];
+
+            itemIDs = [];
 
             for (var i = 0; i < dealDetailData.length; i++) {
                 var dealDetailID = dealDetailData[i]["id"];
+                itemIDs.push(dealDetailID);
                 var dealDetailDrinkNames = dealDetailData[i]["drink_names"];
                 var drinkNamesOptions = "";
 
                 for (var j = 0; j < dealDetailDrinkNames.length; j++) {
                     drinkNamesOptions += "<div class='row'><input type='radio' name='" + dealDetailID +
-                        "' value='" + dealDetailDrinkNames[j] + "'/>" + dealDetailDrinkNames[j] + "</div>";
+                        "' value='" + dealDetailDrinkNames[j] + "'/>" +
+                        "<span class='drink-name-label'>" + dealDetailDrinkNames[j] + "</span></div>";
                 }
 
-                $(".confirm-drink-name-container").append(drinkNamesOptions +
-                    "<div class='row'><input type='radio' name='" + dealDetailID + "' value='custom'/><input type='text'/></row><hr/>"
+                $(".confirm-drink-name-container").append("<div class='deal-data'>" + drinkNamesOptions +
+                    "<div class='row'><input type='radio' name='" + dealDetailID +
+                    "' value='custom' selected/><input type='text' name='" + dealDetailID + "'/></row></div><hr/>"
                 );
             }
         },
