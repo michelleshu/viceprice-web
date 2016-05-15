d3aa690f02ee03779bd4729041fa3573bfef7bcb
Justin <justintsn10@gmail.com>
2016-05-15 14:30:26 -0400

    Changed data_entry to Capitol Hill

---
+++ queries/mturk_data_results.sql
@@ -0,0 +1,15 @@
+﻿select l.name, ah.*, dd.* from "vp_location" l
+join "vp_location_deals" ld
+on l."id" = ld."location_id"
+join "vp_deal" d
+on ld."deal_id" = d."id"
+join "vp_deal_dealDetails" ddd
+on ddd."deal_id" = d."id"
+join "vp_dealdetail" dd
+on dd."id" = ddd."dealdetail_id"
+join "vp_deal_activeHours" dah
+on dah."deal_id" = d.id
+join "vp_activehour" ah
+on dah."activehour_id" = ah.id
+where d."dealSource" = 2
+order by l.id, ah."dayofweek"
