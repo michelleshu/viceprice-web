8a2d2fb74d4849d82eaf174bc7bb983ece37c5cc
Michelle Shu <shu.michelle.w@gmail.com>
2016-05-15 15:42:08 -0400

    HappyHoursUrl query added

---
+++ vp/management/commands/populatehappyhoururls.py
@@ -0,0 +1,26 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.management.base import BaseCommand
+from vp.models import Location
+from viceprice import settings
+import unicodecsv as csv
+
+class Command(BaseCommand):
+
+    # Write Facebook data from file
+    def update_happyhoursurls(self):
+        with open('HappyHourURLS.csv', 'rb') as datafile:
+            reader = csv.reader(datafile)
+            for row in reader:
+                location_name = row[0]
+                locations = Location.objects.filter(name = location_name).all()
+
+                if len(locations) != 1:
+                    print('Zero or multiple locations returned: ' + str(location_name))
+                    continue
+
+                locations.first().happyHourWebsite= row[1]
+                locations.first().save()
+
+
+    def handle(self, *args, **options):
+        self.update_happyhoursurls()
