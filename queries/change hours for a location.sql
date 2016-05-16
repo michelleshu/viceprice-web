eaa4546beffeaf1ce00a315b1e9bb491a4f4268e
Justin <justintsn10@gmail.com>
2016-05-16 17:39:11 -0400

    Foggy Bottom. Minor CSS changes. Updating URLS

---
+++ queries/change hours for a location.sql
@@ -0,0 +1,10 @@
+﻿select ah.* from "vp_location_deals" ld
+join "vp_deal_activeHours" dah
+on ld."deal_id" = dah."deal_id"
+join "vp_activehour" ah
+on dah."activehour_id" = ah."id"
+where ld."location_id" = 2805
+
+update "vp_activehour" ah
+set "start" = '16:00'
+where ah."id" IN (3773, 3772, 3771, 3770, 3769, 3768, 3767)
