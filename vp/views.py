a195de4c762b6e4b8b18dc385ae7e0502985b655
Justin <justintsn10@gmail.com>
2016-05-17 16:31:00 -0400

    Further integration of Wordpress
    Added in a plugin that allows PostGres integration [wordpress only plays
    nice with MySql]

--- vp/views.py
+++ vp/views.py
@@ -233,6 +233,11 @@ def sandbox(request):
     context.update(csrf(request))
     return render_to_response('sandbox.html', context)
 
+def blog(request):
+    context = {}
+    context.update(csrf(request))
+    return render_to_response('blog.html', context)
+
 def index(request):
     context = {}
     context.update(csrf(request))
