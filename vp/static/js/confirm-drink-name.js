6a9829422a4fd8ed759f8f8fdf343387413d1778
Michelle Shu <shu.michelle.w@gmail.com>
2016-07-23 13:42:20 -0400

    Enable removal of one deal detail

--- vp/static/js/confirm-drink-name.js
+++ vp/static/js/confirm-drink-name.js
@@ -4,6 +4,20 @@ var itemIDs = [];
 $(document).ready(function() {
     get_deal_that_needs_confirmation();
 
+    $(document).on("click", ".delete-deal-detail", function(e) {
+        var dealDetailId = parseInt($(e.target).attr("data-deal-detail-id"));
+
+        $.ajax({
+            type: "POST",
+            url: "/delete_deal_detail/",
+            data: JSON.stringify({ "id": dealDetailId }),
+            success: function() {
+                $(".delete-deal-detail[data-deal-detail-id=" + dealDetailId + "]").parent().parent().remove();
+            }
+        });
+    });
+
+
     $("#submit-button").click(function(event) {
 
         event.preventDefault();
@@ -39,7 +53,7 @@ $(document).ready(function() {
             error: function() {
                 alert("Failed to submit names");
             }
-        })
+        });
     })
 });
 
@@ -59,6 +73,7 @@ var get_deal_that_needs_confirmation = function() {
         url: "/get_deal_that_needs_confirmation",
 
         success: function(data) {
+            var locationName = data["location_name"];
             var dealHoursData = data["deal_hour_data"];
             var dealDetailData = data["deal_detail_data"];
             dealID = data["deal_id"];
@@ -78,22 +93,47 @@ var get_deal_that_needs_confirmation = function() {
                             moment(data.end, "HH:mm:ss").format("h:mmA") + "</p>";
                 }
 
+                $(".confirm-drink-name-container").append("<h5 class='location-name'>" + locationName +
+                    "</h5><h6 class='deal-hours'>" + dealHours + "</h6><hr/>");
+
                 itemIDs = [];
                 for (var i = 0; i < dealDetailData.length; i++) {
-                    var dealDetailID = dealDetailData[i]["id"];
-                    itemIDs.push(dealDetailID);
-                    var dealDetailDrinkNames = dealDetailData[i]["drink_names"];
+                    var dealDetail = dealDetailData[i];
+
+                    itemIDs.push(dealDetail.id);
+
+                    var drinkCategory = "";
+                    if (dealDetail.drink_category === 1) {
+                        drinkCategory = "Beer";
+                    } else if (dealDetail.drink_category === 2) {
+                        drinkCategory = "Wine";
+                    } else if (dealDetail.drink_category === 3) {
+                        drinkCategory = "Liquor";
+                    }
+
+                    var dealValue = "";
+                    if (dealDetail.detail_type === 1) {
+                        dealValue = "$" + dealDetail.value;
+                    } else if (dealDetail.detail_type === 2) {
+                        dealValue = dealDetail.value + "% off";
+                    } else if (dealDetail.detail_type === 3) {
+                        dealValue = "$" + dealDetail.value + " off";
+                    }
+                    var dealDetailDrinkNames = dealDetail["drink_names"];
                     var drinkNamesOptions = "";
 
                     for (var j = 0; j < dealDetailDrinkNames.length; j++) {
-                        drinkNamesOptions += "<div class='row'><input type='radio' name='" + dealDetailID +
+                        drinkNamesOptions += "<div class='row'><input type='radio' name='" + dealDetail.id +
                             "' value='" + dealDetailDrinkNames[j] + "'/>" +
                             "<span class='drink-name-label'>" + dealDetailDrinkNames[j] + "</span></div>";
                     }
 
-                    $(".confirm-drink-name-container").append("<div class='deal-data'>" + dealHours + drinkNamesOptions +
-                        "<div class='row'><input type='radio' name='" + dealDetailID +
-                        "' value='custom' selected/><input type='text' name='" + dealDetailID + "'/></row></div><hr/>"
+                    $(".confirm-drink-name-container").append("<div class='deal-data'>" +
+                        "<h6>" + drinkCategory + " - " + dealValue +
+                        "<i class='fa fa-minus-circle delete-deal-detail' data-deal-detail-id='" + dealDetail.id + "'></i></h6>" +
+                        drinkNamesOptions +
+                        "<div class='row'><input type='radio' name='" + dealDetail.id +
+                        "' value='custom' selected/><input type='text' name='" + dealDetail.id + "'/></row></div><hr/>"
                     );
                 }
             }
