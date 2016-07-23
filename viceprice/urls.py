3816740f802811290162a89eaf6005942386e3e0
Justin <justintsn10@gmail.com>
2016-07-23 14:23:04 -0400

    Added Robots.txt and removed wordpress credentials

--- viceprice/urls.py
+++ viceprice/urls.py
@@ -4,6 +4,7 @@ from vp.views import BlogProxyView
 admin.autodiscover()
 
 import vp.views
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     url(r'^$', vp.views.index, name='index'),
@@ -43,4 +44,7 @@ urlpatterns = patterns('',
 
     #Blog site
     url(r'^blog/(?P<path>.*)$', BlogProxyView.as_view())
+                       
+    #Robot
+    url(r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'})
 )
