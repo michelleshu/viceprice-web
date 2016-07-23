028fc826f5ba4d5ca7da44a05d65d1b84da6fd19
Michelle Shu <shu.michelle.w@gmail.com>
2016-07-23 15:46:57 -0400

    Delete old deal data on update

--- vp/static/js/confirm-drink-name.js
+++ vp/static/js/confirm-drink-name.js
@@ -61,6 +61,7 @@ $(document).ready(function() {
         }
 
         var data = {
+            "locationID": locationID,
             "dealID": dealID,
             "namesSelected": namesSelected
         };
