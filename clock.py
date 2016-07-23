c395240386225ba35c3afe7b869b3ab3ae759f86
Michelle Shu <shu.michelle.w@gmail.com>
2016-07-23 14:37:10 -0400

    Revert "Test"
    This reverts commit 2305d56eb357a3f525c8a3da62a3e6961d07a89e.

--- clock.py
+++ clock.py
@@ -1,23 +1,23 @@
-#from apscheduler.schedulers.blocking import BlockingScheduler
-#from rq import Queue
-#from worker import conn
-#from vp.management.commands import updatemturk, getfacebookdata
-#import pytz
-#import logging
-#
-#sched = BlockingScheduler()
-#logging.basicConfig()
-#eastern_timezone = pytz.timezone('US/Eastern')
-#q = Queue(connection = conn)
-#
-#@sched.scheduled_job('cron', minute='0-59/10', second='0', timezone=eastern_timezone)
-#def queue_mturk_update():
-#    print('MTurk update queued')
-#    q.enqueue(updatemturk.run)
-#    
-#@sched.scheduled_job('cron', hour='0', minute='0', second='0', timezone=eastern_timezone)
-#def queue_facebook_update():
-#    print('Facebook update queued')
-#    q.enqueue(getfacebookdata.run)
-#
-#sched.start()
+from apscheduler.schedulers.blocking import BlockingScheduler
+from rq import Queue
+from worker import conn
+from vp.management.commands import updatemturk, getfacebookdata
+import pytz
+import logging
+
+sched = BlockingScheduler()
+logging.basicConfig()
+eastern_timezone = pytz.timezone('US/Eastern')
+q = Queue(connection = conn)
+
+@sched.scheduled_job('cron', minute='0-59/10', second='0', timezone=eastern_timezone)
+def queue_mturk_update():
+    print('MTurk update queued')
+    q.enqueue(updatemturk.run)
+    
+@sched.scheduled_job('cron', hour='0', minute='0', second='0', timezone=eastern_timezone)
+def queue_facebook_update():
+    print('Facebook update queued')
+    q.enqueue(getfacebookdata.run)
+
+sched.start()
