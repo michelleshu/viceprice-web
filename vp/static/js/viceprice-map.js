9b507a6997b7f9b0a81a41314f37c7fad0330577
Justin <justintsn10@gmail.com>
2016-09-24 15:00:46 -0400

    #8

--- vp/static/js/viceprice-map.js
+++ vp/static/js/viceprice-map.js
@@ -459,7 +459,7 @@ markerLayer.on('layeradd', function(e) {
 
     markup += "<div class='mobile-item-metadata'><ul>";
     markup += "<li><a href='tel:" + properties["phoneNumber"]+"'>" + properties["phoneNumber"] + "</a></li>";
-    markup += "<li><a href='https://www.google.com/maps/dir/Current+Location/" + properties["latitude"] + , + properties["longitude"]+"'>" + properties["street"] + "</a></li>";
+    markup += "<li><a href='https://www.google.com/maps/dir/Current+Location/" + properties["latitude"] , + properties["longitude"]+"'>" + properties["street"] + "</a></li>";
     
     ///markup += "<li>" + properties["street"] + "</li>";
     markup += "<li><u><a href='" + properties["website"] + "' target='_blank'>" + properties["website"] + "</a></u></li></ul></div>";
