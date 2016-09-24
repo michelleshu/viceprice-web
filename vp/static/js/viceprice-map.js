a1ec501e35ecb58c1f5cda6cbbf4d7f5addc6fd7
Justin <justintsn10@gmail.com>
2016-09-24 14:38:47 -0400

    Map app test

--- vp/static/js/viceprice-map.js
+++ vp/static/js/viceprice-map.js
@@ -459,7 +459,9 @@ markerLayer.on('layeradd', function(e) {
 
     markup += "<div class='mobile-item-metadata'><ul>";
     markup += "<li><a href='tel:" + properties["phoneNumber"]+"'>" + properties["phoneNumber"] + "</a></li>";
-    markup += "<li>" + properties["street"] + "</li>";
+    markup += "<li><geo:properties["lattitude"],properties["longitude"]+>" + properties["street"] + "</li>";
+    
+    ///markup += "<li>" + properties["street"] + "</li>";
     markup += "<li><u><a href='" + properties["website"] + "' target='_blank'>" + properties["website"] + "</a></u></li></ul></div>";
 
     markup += "<div class='mobile-item-close image-rotate'><img src='static/img/downarrow.svg' width='28' height='28'></div>"
