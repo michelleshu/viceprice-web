b0920cccb2cbd4c1ea4e98fffc42a4e3b05d72a7
Justin <justintsn10@gmail.com>
2016-10-10 17:38:28 -0400

    Whatever biz emails

--- vp/management/commands/populatebusinessemails.py
+++ vp/management/commands/populatebusinessemails.py
@@ -8,14 +8,14 @@ class Command(BaseCommand):
 
     # Write business email data from file
     def update_businessemails(self):
-        with open('vp/management/commands/BizEmailsSep17.csv', 'rb') as datafile:
+        with open('vp/management/commands/BizEmailsOct10.csv', 'rb') as datafile:
             reader = csv.reader(datafile)
             for row in reader:
                 location_id = row[0]
                 location = Location.objects.get(id = location_id)
                 
                 if (location != None):
-                    location.businessEmail = row[4]
+                    location.businessEmail = row[1]
                     location.save()
                     print('Saved Business Email for ' + location.name)
 
