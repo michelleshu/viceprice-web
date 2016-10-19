170f4e5890d398a4ef6a4fdc9a31d94e427c1e4b
Justin <justintsn10@gmail.com>
2016-10-18 20:17:23 -0400

    HH URL Edit

--- vp/management/commands/populatehappyhoururls.py
+++ vp/management/commands/populatehappyhoururls.py
@@ -8,13 +8,14 @@ class Command(BaseCommand):
 
     # Write happy hour URL data from file
     def update_happyhoursurls(self):
-        with open('vp/management/commands/UpdateHHURLSJIRA.csv', 'rb') as datafile:
+        with open('vp/management/commands/HHURLOCT18.csv', 'rb') as datafile:
             reader = csv.reader(datafile)
             for row in reader:
                 location_id = row[0]
                 location = Location.objects.get(id = location_id)
                 if (location != None):
-                    location.happyHourWebsite = row[4]
+                    #BEWARE OF WHICH COLUMN INFO IS ON]
+                    location.happyHourWebsite = row[1]
                     location.save()
                     print('Saved Happy Hour URL for ' + location.name)
 
